9e1421ac8769808745f89e06c9b67b9d110f3cf5
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-05-13 19:10:38 -0400

    final

--- src/components/Jumbotron/jumbotron.js
+++ src/components/Jumbotron/jumbotron.js
@@ -4,26 +4,30 @@ import "./jumbotron.css";
 
 const Jumbotron = props => (
   
-  <nav>
-    <ul>
-      <li className="brand animated wobble">
-        <span className = "header">{props.title}</span>
-      </li>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+      <ul>
+        <li className="brand animated wobble">
+          <span className="header">{props.title}</span>
+        </li>
 
-      <li id="message">{props.message}</li>
+        <li id="message">{props.message}</li>
 
-      <li id="gameScore">Current Score: {props.score}</li>
+        <li id="gameScore">Current Score: {props.score}</li>
 
-      <li id="highScore">Top Score: {props.highScore}</li>
-    </ul>
-    <br></br>
-  </nav>
+        <li id="highScore">Top Score: {props.highScore}</li>
+      </ul>
+  
+    </div>
+  </div>
+ 
+ 
 
 
 
     );
     
-    
+
     
     
     
